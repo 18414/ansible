486b1780b018ae20240d450310345f0dae8674e0
bhushan mahajan <bmahajan0@gmail.com>
2019-04-04 01:40:39 +0000

    2019-04-04

--- playbooks/ec2_gym_system_infra.sh
+++ playbooks/ec2_gym_system_infra.sh
@@ -16,9 +16,17 @@ grep [0-9] hosts > ip_list
 
 sleep 1
 
+#Launching RDS 
+
+
+
+#inserting tables
+sudo mysql -h `cat rds-hostname.txt` -u bhushan -p ganesha123 < gym_management_system.sql
 
 #Launching containers 
 ansible-playbook docker_launch_xampp_v2_new.yml --extra-vars "instance1=`cat ip_list| head -1` instance2=`cat ip_list| tail -1` paswd=ganesha@123"
 
 
 
+hm=`cat rds-hostname.txt`
+sed "s/127.0.0.1/${hm}/g; s/root/bhushan/g; s/""/"ganesha123"/g"  /oplampp/htdocs/gym_management_system/includes/db_connect.php
