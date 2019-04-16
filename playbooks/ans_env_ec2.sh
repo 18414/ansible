ec8150fa92e42f54a6d08628a771e35e64f28775
Bhushan Mahajan <bmahajan0@gmail.com>
2019-04-16 04:12:12 +0000

    2019-04-16

--- playbooks/ans_env_ec2.sh
+++ playbooks/ans_env_ec2.sh
@@ -1,38 +1,30 @@
 #!/bin/bash
- 
+
  #Adding user
  useradd ansible
- 
+
  #SUDO access
  sed -i "/^root/a \\ansible ALL=(ALL) NOPASSWD: ALL" /etc/sudoers > /dev/null
 
- #Creating SSH keygen 
- echo -e -n "`tput setaf 2``tput bold`\nCreating ssh keys for user $user_name `tput sgr0`";sleep 2;echo -n ..;sleep 1;echo -n ...;echo " ";echo " "
+ #Creating SSH keygen
     su - ansible << EOF
-    echo -e "\n"|ssh-keygen -t rsa -N ""    
+    echo -e "\n"|ssh-keygen -t rsa -N ""
 EOF
 
  #Installing Ansible
- rpm -qa | grep ansible > /dev/null
-  if [ $? -eq 0 ]; then
 
-   echo -e "`tput setaf 6`Ansible is installed with version `ansible --version | head -1 | awk -F " " '{print $2}'``tput sgr0`"
-   pause
-  else
-   echo -e -n "`tput setaf 2``tput bold`\nInstalling Ansible $main_var `tput sgr0`";sleep 2;echo -n ..;sleep 1;echo -n ...;echo " ";echo " "
-
-      yum install epel-release -y > /dev/null
-      yum install ansible -y  /dev/null
-  fi
+  yum install epel-release -y > /dev/null
+  yum install ansible -y  /dev/null
+  yum install git -y
+  #git config --global user.name "Bhushan Mahajan"
+  #git config --global user.email "bmahajan0@gmail.com"
 
   ## Git clone
-  yum install git -y 
-  git config --global user.name "Bhushan Mahajan"
-  git config --global user.email "bmahajan0@gmail.com"  
 
-  su - ansible 
-    git clone https://github.com/18414/ansible.git
-  
-    echo " cd /home/ansible/ansible/playbooks" >> ~/.bashrc
-  
-  
+  su - ansible << EOF
+  git config --global user.name "Bhushan Mahajan"
+  git config --global user.email "bmahajan0@gmail.com"
+  git clone https://github.com/18414/ansible.git
+EOF
+
+
