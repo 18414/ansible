5acccdc5c3804f9ee877df36fef858e781196a0a
Bhushan Mahajan <bmahajan0@gmail.com>
2019-04-16 04:27:27 +0000

    2019-04-16

--- playbooks/ans_env_ec2.sh
+++ playbooks/ans_env_ec2.sh
@@ -1,16 +1,23 @@
-asdsasasasasa
+#!/bin/bash
 
-
-a
-sdsa
-d
-asd
-asd
-as
-dasddding user
+ #Adding user
  useradd ansible
 
  #SUDO access
+ sed -i "/^root/a \\ansible ALL=(ALL) NOPASSWD: ALL" /etc/sudoers > /dev/null
+
+ #Creating SSH keygen
+    su - ansible << EOF
+    echo -e "\n"|ssh-keygen -t rsa -N ""
+EOF
+
+ #Installing Ansible
+
+  yum install epel-release -y > /dev/null
+  yum install ansible -y  /dev/null
+  yum install git -y
+  #git config --global user.name "Bhushan Mahajan"
+  #git config --global user.email "bmahajan0@gmail.com"
 
   ## Git clone
 
