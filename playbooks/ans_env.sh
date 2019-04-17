45c993a559e11f2933d77147a9cde1c8e99dd444
Bhushan Mahajan <bmahajan0@gmail.com>
2019-04-17 14:49:21 +0000

    2019-04-17

--- playbooks/ans_env.sh
+++ playbooks/ans_env.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
  useradd ansible
  mkdir -p /home/ansible/.ssh
-     echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC92Ihz4c0UgL78rV83XyFByuY3PP0bXxl70DFBNoUL/kPjLjGgYnfAmQRovIHaLJMUybQj4DoUHk2VtHGRFRx3K78Zo1G1/kiSDqvDWFvYr8gi3tDkXG1mBhuh7owFEHRSsuzITGtyUXp9aEZlM24N7KJ5zJzTYgIbQstVuRWddzzElom9UGwXQXZXMfThnocMCWfj6KJoLgYfzLg6M6gSE5VtrvJa6d7n1CyGryoc9Gz92TPOMXT+qrrKqsr0Pfvp5xmMCUa2efEVYDfot/UDA4hRL8hHHDdNXfmHfZpi0oW7Ew7sLf14d7TVpjpmKy0bNZIgYToSDy04Qxhh/uV7 ansible@ansible.example.com" > /home/ansible/.ssh/authorized_keys
+     echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDjklgtB5N0cxsn3LTFhB/c22/i50RqJ0Zm+5ECbGA+FkxAu6xvpe5ifiAjcFJF5rYz2bQkYEpFGtPJcYPp8+GG3gCOdGvt5dl0+Uh7kJS/z51LbQl3A+BcXr/JqYYFEtqYZ3olIyyQLCVnap57TSJxs8taUmhT0yS61fZoCk0uso4I+tJer3VmO4pQLwuaojZfkNwOu2VIHjkdx2Iv70mEh0h92p/pT8tMJPefUlgN5cF8a1x3ewyXy2B1QKE2PwTHRZTqwR2eDmUXzI/sI3nLvsbZ4CSsrcOb4+4ih9DrIOXveYCxSZZW6f7aJebwl291+VwTi/ztusZM2P+Vdbpn ansible@ip-172-31-45-133.us-east-2.compute.internal" > /home/ansible/.ssh/authorized_keys
  chmod 700 /home/ansible/.ssh
  chmod 600 /home/ansible/.ssh/authorized_keys
  chown -R ansible.ansible /home/ansible/.ssh/authorized_keys
