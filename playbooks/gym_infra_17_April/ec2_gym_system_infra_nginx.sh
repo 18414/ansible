42857ac2fe8299ccc9ebcc092a31f78949d540de
Bhushan Mahajan <bmahajan0@gmail.com>
2019-04-17 12:28:09 +0000

    2019-04-17

---
+++ playbooks/gym_infra_17_April/ec2_gym_system_infra_nginx.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# Version: 1.0
+
+#Deleting previous ip addresses of instances from ANSIBLE inventory
+sed -i '/^[0-9]/d' hosts
+sleep 1
+
+#Launching EC2 instances  
+ansible-playbook gym_mgmt_system_setup_nginx.yml
+
+sleep 1
+
+
+#installing Docker 
+ansible-playbook docker_install_centos.yml
+
+# IP of created instance
+host_ip=$(grep [0-9] hosts)
+
+sleep 1
+
+
+#Launching containers 
+ansible-playbook docker_launch_nginx_lb_v1.yml --extra-vars  "hip=$host_ip paswd=ganesha@123"
+
+
+
