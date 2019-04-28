8d60f8ca3f9baeee27b147cda4698634696dfd79
Bhushan Mahajan <bmahajan0@gmail.com>
2019-04-28 03:57:55 +0000

    2019-04-28

--- playbooks/ec2_ans_env_mini.sh
+++ playbooks/ec2_ans_env_mini.sh
@@ -20,6 +20,8 @@ EOF
   wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat/jenkins.repo
   rpm --import https://pkg.jenkins.io/redhat/jenkins.io.key
   yum install jenkins  java-1.8.0-openjdk-devel -y
+  systemctl enable jenkins
+  systemctl start jenkins
 
 
   ## Git clone
