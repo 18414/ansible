8fdef38bcbab936bd3bf3f23eff34912ef85567d
Bhushan Mahajan <bmahajan0@gmail.com>
2019-07-15 02:26:21 +0000

    new jenkins file

--- playbooks/jenkins_user_data.sh
+++
@@ -1,5 +0,0 @@
-       yum install -y wget
-      wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat/jenkins.repo
-      rpm --import https://pkg.jenkins.io/redhat/jenkins.io.key
-      yum install jenkins  java-1.8.0-openjdk-devel git -y
-
