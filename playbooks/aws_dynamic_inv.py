376120e905ca23560dee88ac34dea6b8bbb71056
bhushan <bmahajan0@gmail.com>
2019-08-09 06:27:29 +0000

    Ansible dynamic inventory

--- playbooks/aws_dynamic_inv.py
+++ playbooks/aws_dynamic_inv.py
@@ -2,6 +2,7 @@
 
 import boto3
 import json
+import pprint 
 
 def get_hosts(ec2,fv):
   f={'Name': 'tag:Ansible','Values':[fv]}
@@ -15,8 +16,27 @@ def main():
   ec2=boto3.resource("ec2")
   db_group=get_hosts(ec2, "db")
   app_group=get_hosts(ec2,"App")
-  print "db: ",db_group
-  print "app:",app_group
-  
+#  print "db: ",db_group
+ # print "App:",app_group
+ 
+  all_groups= {
+              'db': {
+                 'hosts':db_group,
+                 'vars': {
+                       'group_name': 'Database group'
+                         }     
+                    },
+               'App': {
+                  'hosts': app_group,
+                  'vars': {
+                       'group_name': 'Application group'
+                          }
+                      }
+              }
+
+ # pprint.pprint ( all_groups )
+  print json.dumps(all_groups)
+
+
 if __name__=="__main__":
    main()
