9812dcb7eb86eb91b5bd6e29172b462f9f013730
Bhushan Mahajan <bmahajan0@gmail.com>
2020-01-25 10:38:02 +0000

    removed unuse file

---
+++ playbooks/async_poll.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+echo "Long running Job is beginer"
+echo "My Job is to Sleep"
+echo "Now am going to Sleep for $1 Seconds"
+sleep $1
+echo "Huuuhhh!!.. What time is It?"
+echo "Can I get some Bed Coffee please"
+exit
